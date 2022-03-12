fa17ccfcd2ddd4a8e7dab0e4cfb92d05af2e448f
MAlngaawy <m.alngaawy@gmail.com>
2022-03-12 23:35:32 +0200

    remove swiper and use react-res-caro instead

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import 'swiper/components/navigation/navigation.min.css';
 // import required modules
 import SwiperCore, { Pagination, Navigation } from 'swiper/core';
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
 SwiperCore.use([Pagination, Navigation]);
 
 const api = 'https://mo-strapi-playground.herokuapp.com';
@@ -38,46 +41,37 @@ export default App;
 const Home = () => {
   const test = ['One', 'Two', 'Three', 'Four', 'Five'];
   return (
-    <Swiper
-      loop={true}
-      pagination={{
-        clickable: true
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation]}
-      slidesPerView={1}
-    >
-      {/* 
-      
-        // Test Here
-
-         grid grid-flow-col justify-evenly
-      
-      */}
-      {test.map(item => (
-        <SwiperSlide key={item} className="p-10">
-          <div className="slide bg-gray bg-gray-400 mx-10 grid grid-flow-col">
-            <div className="image">
-              <img src="https://picsum.photos/id/10/200/300" alt="" />
+    <>
+      <h1>react responsive carousel</h1>
+      <div className="carouselcontainer">
+        <Carousel>
+          {test.map(item => (
+            <div
+              key={item}
+              className="slide bg-gray bg-gray-400 mx-10 grid grid-flow-col m-10"
+            >
+              <div className="image">
+                <img src="https://picsum.photos/id/10/200/300" alt="" />
+              </div>
+              <div className="info p-10  place-self-center">
+                <h3 className=" uppercase tracking-wider ">Editor's pick</h3>
+                <h2 className="text-xl font-bold leading-normal">
+                  News Needs to Meet Its Audiences Where They Are
+                </h2>
+                <h2>This is a small text</h2>
+                <p>
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Inventore excepturi facilis similique, nulla expedita quos
+                  dolorem non officia odio neque deserunt, possimus veniam
+                  maiores aliquid. Fugiat ea aspernatur doloribus natus.{' '}
+                </p>
+              </div>
             </div>
-            <div className="info p-10  place-self-center">
-              <h3 className=" uppercase tracking-wider ">Editor's pick</h3>
-              <h2 className="text-xl font-bold leading-normal">
-                News Needs to Meet Its Audiences Where They Are
-              </h2>
-              <h2>This is a small text</h2>
-              <p>
-                {' '}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore excepturi facilis similique, nulla expedita quos
-                dolorem non officia odio neque deserunt, possimus veniam maiores
-                aliquid. Fugiat ea aspernatur doloribus natus.{' '}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          ))}
+        </Carousel>
+      </div>
+    </>
   );
 };
 export { api };
